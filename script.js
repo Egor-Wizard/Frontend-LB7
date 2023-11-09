6a9504721212e62cf59c67ad09568e6e55bd7372
Egor-Wizard <118670640+Egor-Wizard@users.noreply.github.com>
2023-11-09 16:54:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const magicBallImage = document.getElementById("magic-ball-image");
+    const magicBall = document.querySelector(".magic-ball");
 
     magicBallImage.addEventListener("click", function () {
-        const userQuestion = prompt("Поставте питання:");
+        const userQuestion = prompt("Задайте питання:");
         provideAnswer(userQuestion);
     });
 
     function provideAnswer(question) {
         if (question) {
-            const answers = ["Так", "Ні"];
+            // Додайте клас з анімацією під час введення питання
+            magicBall.classList.add("shake");
+
+            const answers = ["Так", "Ні", "Не знаю", "Можливо, так, а може, й ні", "Ні, я не вважаю так"];
             const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-            alert(`Магічна куля відповідає: ${randomAnswer}`);
+            
+            setTimeout(function () {
+                alert(`Магічна куля відповідає: ${randomAnswer}`);
+                // Видаліть клас анімації після відповіді
+                magicBall.classList.remove("shake");
+            }, 500); // Час анімації 0.5 секунди
         }
     }
 });
